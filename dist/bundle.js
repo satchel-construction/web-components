d6fc94dcaebc1c3cd579f739d4d6c7f3e86cc370
NFuller721 <noahfuller@Prawars-iMac.localdomain>
2024-06-12 09:46:38 -0400

    dist

---
+++ dist/bundle.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+
+  class Title extends HTMLElement {
+    constructor() {
+      super();
+
+      const element = document.createElement("h1");
+      element.innerText = "Hello World!";
+
+      const shadowRoot = this.attachShadow({ mode: "open" });
+      shadowRoot.appendChild(element);
+    }
+  }
+
+  window.customElements.define("st-title", Title);
+
+})();
