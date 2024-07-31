fd65bff70f9323f2213a9454e34652d53cfbd226
NFuller721 <noahfuller@Prawars-iMac.localdomain>
2024-07-31 11:38:39 -0400

    made a mistake

--- src/form/tagged-select.js
+++ src/form/tagged-select.js
@@ -65,7 +65,7 @@ export default class TaggedSelect extends Select {
       text.innerText = tag.chip;
 
       tagElement.className = tag.active === false ? "badge badge-ghost" : "badge badge-neutral";
-      tagElement.onclick = () => cb(tag);
+      tagElement.onclick = () => this.tagClick(tag);
       tagElement.appendChild(text);
       tagElement.appendChild(remove);
 
