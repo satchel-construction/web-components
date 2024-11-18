6bf98ecf0adf3b6a19e18e5528c9e1e57a257096
NFuller721 <noahfuller@Prawars-iMac.localdomain>
2024-11-18 15:17:52 -0500

    onSelect

--- src/form/select.js
+++ src/form/select.js
@@ -162,6 +162,8 @@ export default class Select extends HTMLElement {
 
   /** @param {Option} option */
   select(option) {
+    console.log(option);
+
     this.searchField.value = option.title;
     this._internals.setFormValue(option.value);
     this._value = option.value;
